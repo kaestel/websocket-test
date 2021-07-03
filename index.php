0606864115c9f38eaf4c170a0f44742738ffcbca
Martin Kæstel Nielsen <martin@think.dk>
2021-07-03 11:42:12 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+//http://phppot.com/php/simple-php-chat-using-websocket/
+?><html>
+<head>
+	<style>
+	body{width:600px;font-family:calibri;}
+	.error {color:#FF0000;}
+	.chat-connection-ack{color: #26af26;}
+	.chat-message {border-bottom-left-radius: 4px;border-bottom-right-radius: 4px;
+	}
+	#btnSend {background: #26af26;border: #26af26 1px solid;	border-radius: 4px;color: #FFF;display: block;margin: 15px 0px;padding: 10px 50px;cursor: pointer;
+	}
+	#chat-box {background: #fff8f8;border: 1px solid #ffdddd;border-radius: 4px;border-bottom-left-radius:0px;border-bottom-right-radius: 0px;min-height: 300px;padding: 10px;overflow: auto;
+	}
+	.chat-box-html{color: #09F;margin: 10px 0px;font-size:0.8em;}
+	.chat-box-message{color: #09F;padding: 5px 10px; background-color: #fff;border: 1px solid #ffdddd;border-radius:4px;display:inline-block;}
+	.chat-input{border: 1px solid #ffdddd;border-top: 0px;width: 100%;box-sizing: border-box;padding: 10px 8px;color: #191919;
+	}
+	</style>	
+	<script>  
+		function showMessage(messageHTML, type) {
+			var message = window.chatbox.appendChild(document.createElement("div"));
+			message.className = type;
+			message.innerHTML = messageHTML;
+		}
+
+		window.onload = function(){
+			this.chatbox = document.getElementById("chat-box");
+
+			var websocket = new WebSocket("ws://websocket.local:8090/php-socket.php"); 
+			websocket.onopen = function(event) { 
+				showMessage("Connection is established!", "chat-connection-ack");		
+			}
+			websocket.onmessage = function(event) {
+				var Data = JSON.parse(event.data);
+				showMessage(Data.message, Data.message_type);
+				document.getElementById("chat-message").value;
+			};
+		
+			websocket.onerror = function(event){
+				showMessage("Problem due to some Error", "error");
+			};
+			websocket.onclose = function(event){
+				showMessage("Connection Closed", "chat-connection-ack");
+			}; 
+		
+			document.getElementById("frmChat").onsubmit = function(event){
+				event.preventDefault();
+				document.getElementById("chat-user").type = "hidden";		
+				console.log(document.getElementById("chat-message"));
+				var messageJSON = {
+					chat_user: document.getElementById("chat-user").value,
+					chat_message: document.getElementById("chat-message").value
+				};
+				websocket.send(JSON.stringify(messageJSON));
+			};
+		};
+	</script>
+
+	</script>
+	</head>
+	<body>
+		<form name="frmChat" id="frmChat">
+			<div id="chat-box"></div>
+			<input type="text" name="chat-user" id="chat-user" placeholder="Name" class="chat-input" required />
+			<input type="text" name="chat-message" id="chat-message" placeholder="Message"  class="chat-input chat-message" required />
+			<input type="submit" id="btnSend" name="send-chat-message" value="Send" >
+		</form>
+</body>
+</html>
